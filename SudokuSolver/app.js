197b6cc2951dbb7bf9c915a39330c19e5404f8f2
Riteris13 <riteris13@gmail.com>
2018-07-15 02:55:13 +0100

    ejs and dynamodb

--- SudokuSolver/app.js
+++ SudokuSolver/app.js
@@ -4,14 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//app.get('/', (req, res) => res.send('<html>< header > <title>This is title</title></header ><body>Hello world</body></html >'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +38,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('pages/error');
 });
 
 module.exports = app;
--- SudokuSolver/routes/users.js
+++ SudokuSolver/routes/users.js
@@ -1,9 +1,38 @@
 var express = require('express');
+
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+// Load the SDK for JavaScript
+var AWS = require('aws-sdk');
+
+AWS.config.update({ region: 'eu-west-1' });
+
+// Create DynamoDB document client
+var dynamoDb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+
+
+router.get('/', function (req, res, next) {
+    dynamoDb.scan({ TableName: 'allusers' }, function (err, data) {
+        if (err) {
+            res.status("500").send(err);
+        } else {
+            res.status("200").send(data);
+        }
+    });
+});
+
+router.put('/', function (req, res, next) {
+    dynamoDb.put({
+        TableName: 'allusers',
+        Item: { userid: '124', name: 'deadpool' }
+    }, function (err, data) {
+        if (err) {
+            res.status("500").send(err);
+        } else {
+            res.status("200").send(data);
+        }
+    });
 });
 
 module.exports = router;
