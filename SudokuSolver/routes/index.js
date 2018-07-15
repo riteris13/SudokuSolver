6b985fb916ba49037278676a7df84ea323d4629c
Riteris13 <riteris13@gmail.com>
2018-07-15 22:48:47 +0100

    urlFix

--- SudokuSolver/routes/index.js
+++ SudokuSolver/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'Vlads Sudoku Solver'});
+  res.render('pages/index', { title: 'Vlads Sudoku Solver', url: req.path });
 });
 
 module.exports = router;
